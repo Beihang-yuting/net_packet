8e31f4e67d30fed3797f9c07f744b61407925817
Developer <developer@net-packet.local>
2026-04-13 15:17:40 +0800

    feat: update filelist.f with all Phase 1 source files

--- filelist.f
+++ filelist.f
@@ -9,3 +9,15 @@
 src/common/packet_defines.sv
 src/common/packet_utils.sv
 src/protocols/protocol_base.sv
+src/protocols/l2/eth_header.sv
+src/protocols/l2/vlan_header.sv
+src/protocols/l3/ipv4_header.sv
+src/protocols/l3/ipv6_header.sv
+src/protocols/l3/arp_header.sv
+src/protocols/l4/tcp_header.sv
+src/protocols/l4/udp_header.sv
+src/protocols/l4/icmp_header.sv
+src/protocols/l4/icmpv6_header.sv
+src/core/protocol_graph.sv
+src/core/template_registry.sv
+src/core/packet.sv
