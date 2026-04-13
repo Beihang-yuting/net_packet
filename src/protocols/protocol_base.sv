30a2938954d3d9d8bb62c9fc1b9602f807bb294f
Developer <developer@net-packet.local>
2026-04-13 14:59:19 +0800

    feat: add protocol_base abstract class with virtual interface

---
+++ src/protocols/protocol_base.sv
@@ -0,0 +1,31 @@
+// src/protocols/protocol_base.sv
+`ifndef PROTOCOL_BASE_SV
+`define PROTOCOL_BASE_SV
+
+`include "packet_defines.sv"
+`include "packet_utils.sv"
+
+virtual class protocol_base;
+
+    protocol_type_e  proto_type;
+    bit              auto_calc = 1;
+
+    pure virtual function void pack_header(ref byte unsigned data[$]);
+    pure virtual function void unpack_header(ref byte unsigned data[$], ref int offset);
+    pure virtual function int get_header_length();
+    pure virtual function void calc_fields(byte unsigned payload_data[$], protocol_type_e next_proto);
+    pure virtual function protocol_base clone();
+    pure virtual function bit compare(protocol_base other);
+    pure virtual function string to_string();
+    pure virtual function string to_brief();
+
+    function void print(int verbosity = 0);
+        if (verbosity == 0)
+            $display("  [%s] %s", proto_type.name(), to_brief());
+        else
+            $display("%s", to_string());
+    endfunction
+
+endclass
+
+`endif // PROTOCOL_BASE_SV
